6084f55516ad0a4b3b6c6981ad5b5f0616e4557f
Joshua Tzucker <joshua.tz@gmail.com>
2019-01-23 16:08:25 -0800

    component base

---
+++ src/components/editor/panels/EffectsEditor.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class EffectsEditor extends Component {
+  constructor(props){
+    super(props);
+    let initialState = {};
+    this.state = initialState;
+  }
+
+  render(){
+    return (
+      <div className="EffectsEditorPanel">
+
+      </div>
+    )
+  }
+}
