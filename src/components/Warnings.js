52cf3d903282ec6ddfe679be816265e66af0d31c
Joshua Tzucker <joshua.tz@gmail.com>
2019-01-28 23:03:28 -0800

    Added warning for URL character count

--- src/components/Warnings.js
+++ src/components/Warnings.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 class Warnings extends Component {
   constructor(props){
     super(props);
+    this.recommendedMaxUrlLength = 2000;
   }
 
   render(){
@@ -11,7 +12,13 @@ class Warnings extends Component {
         {this.props.masterState.accountSettings.fetchInstantly &&
           <div className="warningBar">
             <div className="warningTitle">Instant Preview is ON - Consumes More Credits</div>
-            <div className="warningMoreInfo"><i class="material-icons" data-tooltip="When instant preview is on, a new preview will be fetched from Cloudinary every time you make an edit. This can result in using hundreds of transformations within minutes, using up a lot of credits.">info</i></div>
+            <div className="warningMoreInfo"><i className="material-icons" data-tooltip="When instant preview is on, a new preview will be fetched from Cloudinary every time you make an edit. This can result in using hundreds of transformations within minutes, using up a lot of credits.">info</i></div>
+          </div>
+        }
+        {this.props.masterState.output.img.src.length > this.recommendedMaxUrlLength && 
+          <div className="warningBar">
+            <div className="warningTitle">Generated URL Length > {this.recommendedMaxUrlLength}</div>
+            <div className="warningMoreInfo"><i className="material-icons" data-tooltip="2083 is the max character count for URLs for IE, so 2K is good guideline if going to use URL for client-facing stuff">info</i></div>
           </div>
         }
       </div>
